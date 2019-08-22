71aad45ae28120e90575250ab0aede177af3f3d0
Juan Felipe Rios <juanf-riosc@unilibre.edu.co>
2019-08-22 00:52:08 -0500

    Agrege informacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,12 +28,24 @@ export class AppComponent {
    }
   
   division(){
+    if(this.campo1 == 0)
+    {      
+    alert ("no se puede dividir por cero")
+    return null;
+    }
+    else
     this.rta=this.campo/this.campo1;
    }
 
   modulo(){
   this.rta = this.campo%this.campo1;
  }
+ Reiniciar()
+  {
+    this.campo =  null ;
+    this.campo1 =  null ;
+    this.rta =  null ;
+  }
   
   
 
